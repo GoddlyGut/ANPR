5bd3b18c2364510bdb0cae43b7d81425ba1c8664
GoddlyGut <a.reitman@icloud.com>
2022-07-19 09:57:45 +0100

    update

---
+++ ANPR/Core/ANPR.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using LSPD_First_Response.Mod.API;
+using LSPD_First_Response.Engine.Scripting.Entities;
+using Rage;
+
+namespace ANPR.Core
+{
+    public class ANPR
+    {
+        public ANPR()
+        {
+        }
+    }
+}
+
